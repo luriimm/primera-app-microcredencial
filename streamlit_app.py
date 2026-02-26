ea58ed0b2f2040bdad298227f602268c2d3becc5
Laura Marrero <168206337+luriimm@users.noreply.github.com>
2026-02-26 10:09:24 +0000

    primer cambio

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Primera app")
+st.balloons()
+st.text('Hola Bentor, buenos días')
